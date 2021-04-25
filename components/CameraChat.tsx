61966dda812ec39a6a4ddba5fca9b60a63f89a08
Justin Turner <turnerjw@rogers.com>
2021-04-25 16:05:32 -0400

    Added camerabox

---
+++ components/CameraChat.tsx
@@ -0,0 +1,9 @@
+export function CameraChat() {
+  return (
+    <div className="flex-col w-96 h-96 bg-black bg-opacity-70 rounded-6xl shadow-2xl">
+      <div className="bg-black flex-none text-white rounded-t-6xl text-center h-1/2">
+        Camera Drastic Go Here
+      </div>
+    </div>
+  );
+}
---
+++ pages/camerabox.tsx
@@ -0,0 +1,11 @@
+import { CameraChat } from "../components/CameraChat";
+
+export default function Chabox() {
+  return (
+    <div className="flex">
+      <div className="p-20 flex-none">
+        <CameraChat />
+      </div>
+    </div>
+  );
+}
