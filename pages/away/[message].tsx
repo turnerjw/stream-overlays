d238a1ab3cfa79c51fb643a53efd0f6c0f958004
Justin Turner <turnerjw@rogers.com>
2021-04-25 19:28:06 -0400

    Created away screen

--- pages/away/[message].tsx
+++ pages/away/[message].tsx
@@ -1,11 +1,16 @@
-import { Floater } from "../components/Floater";
+import { Floater } from "../../components/Floater";
+import { useRouter } from "next/router";
 
 export default function StartingSoon() {
+  const {
+    query: { message },
+  } = useRouter();
+
   return (
     <div className="w-1920 h-1080 bg-gradient-to-tr from-cyan-800 to-rose-500 relative">
       <Floater />
       <p className="absolute text-white bottom-10 right-10 text-8xl">
-        Starting Soon
+        {message}
       </p>
     </div>
   );
