9af368dc6de8281285422a5d0bb07f71745fa363
Justin Turner <turnerjw@rogers.com>
2021-04-26 11:22:31 -0400

    iPad overlay

---
+++ components/Ipad.tsx
@@ -0,0 +1,5 @@
+export function Ipad() {
+  return (
+    <div className="flex-col w-1036 h-720 bg-white rounded-6xl shadow-2xl"></div>
+  );
+}
---
+++ pages/ipad.tsx
@@ -0,0 +1,11 @@
+import { Ipad } from "../components/Ipad";
+
+export default function Chabox() {
+  return (
+    <div className="flex">
+      <div className="p-20 flex-none">
+        <Ipad />
+      </div>
+    </div>
+  );
+}
