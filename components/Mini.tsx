f9e9d400395df1954b5754a02725446df163a07c
Justin Turner <turnerjw@rogers.com>
2021-04-26 15:29:40 -0400

    Created mini screen

---
+++ components/Mini.tsx
@@ -0,0 +1,5 @@
+export function Mini() {
+  return (
+    <div className="flex-col h-540 w-960 bg-white rounded-6xl shadow-2xl"></div>
+  );
+}
---
+++ pages/mini-screen.tsx
@@ -0,0 +1,11 @@
+import { Mini } from "../components/Mini";
+
+export default function MiniScreen() {
+  return (
+    <div className="flex">
+      <div className="p-20 flex-none">
+        <Mini />
+      </div>
+    </div>
+  );
+}
